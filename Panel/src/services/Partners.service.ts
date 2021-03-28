25571a91c327a5d9a9f03b020da3b7232c467247
aanthonyawad <dev.anthony.awad@gmail.com>
2021-03-28 21:14:38 +0300

    added tests

--- Panel/src/services/Partners.service.ts
+++ Panel/src/services/Partners.service.ts
@@ -49,7 +49,7 @@ export const listWithinRange = async (
 export const calculatedistance = (office: Office) => {
   let coords: string[] = office.coordinates.split(",");
   let long = +coords[0];
-  let lat = +coords[0];
+  let lat = +coords[1];
 
   let absoluteDifferenceLat = +(lat - MeetingDetails.lat);
   let absoluteDifferenceLong = +(long - MeetingDetails.long);
---
+++ Panel/test/partner.service.test.ts
@@ -0,0 +1,28 @@
+import { MeetingDetails } from './../src/constants/meetingDetails';
+import * as chai from 'chai';
+
+
+export const calculatedistance = () => {
+    let long = 1.28304;
+    let lat = 103.85199319999992;
+  
+    let absoluteDifferenceLat = +(lat - MeetingDetails.lat);
+    let absoluteDifferenceLong = +(long - MeetingDetails.long);
+  
+    let result =
+      2 *
+      Math.asin(
+        Math.sqrt(
+          Math.sin(absoluteDifferenceLat / 2) *
+            Math.sin(absoluteDifferenceLat / 2) +
+            Math.cos(lat) +
+            Math.cos(MeetingDetails.lat) *
+              Math.sin(absoluteDifferenceLong / 2) *
+              Math.sin(absoluteDifferenceLong / 2)
+        )
+      );
+    if (result != NaN) {
+      return MeetingDetails.earthRadius * result;
+    }
+    chai.expect(result).equal(19.566992494874732);
+  };
